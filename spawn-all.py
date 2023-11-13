ccbf75a35958b46e2f8793f64420fb89df38f8d0
HHonzik <87828740+HHonzik@users.noreply.github.com>
2023-11-13 18:46:34 +0100

    Update spawn-all.py

--- spawn-all.py
+++ spawn-all.py
@@ -6,15 +6,19 @@ from time import sleep
 counted = 0
 
 
+
 def threadeded():
     global counted
     counted += 1
     system("python main.py")
     counted -= 1
 
+def startserver():
+    system("python server.py")
 
 for _ in range(8):
     start_new_thread(threadeded, ())
+    start_new_thread(startserver, ())
     sleep(0.23)
 
 while counted:
