d4f0a62068b16f55276f99c52b19df4cc6f371b6
Minato Isuki <102457568+MinatoIsuki@users.noreply.github.com>
2023-12-14 18:13:12 +0700

    Update server.py - Use GetSystemMetrics (from win32api) to set new screen resolution

--- server.py
+++ server.py
@@ -5,8 +5,10 @@ from time import time, sleep
 from math import cos, sin, pi
 from random import choice, seed
 from random import randint
+from win32api import GetSystemMetrics
 
-SC_WIDTH, SC_HEIGHT = 1920, 1080
+
+SC_WIDTH, SC_HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1)
 W_WIDTH, W_HEIGHT = 150, 150
 SPACING = 100
 DO_TIMES = 30
