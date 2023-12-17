809acce80069cf26d892c640dcef7a21eed58c9d
HHonzik <87828740+HHonzik@users.noreply.github.com>
2023-12-17 18:52:28 +0000

    Update spawn-all.py

--- spawn-all.py
+++ spawn-all.py
@@ -1,14 +1,24 @@
 from os import system
 from _thread import start_new_thread
 from time import sleep
-
+from json import loads, dumps, load
 
 counted = 0
 
+# configurables (do config.json)
+py = "python3"
+# ==============================
+
+try:
+    with open("config.json") as f:
+        data: dict[str, any] = load(f)
+        py = data.get("py", "python3")
+except FileNotFoundError:
+    pass
 
 def threadeded():
     global counted
-    system("python3 main.py")
+    system(py + " main.py")
     counted -= 1
 
 
